0bf3748f2bcc4cbd77380265b5b45d544bdbb5f1
Mardson Ferreira <mardsonferreira25@gmail.com>
2019-04-14 23:31:55 -0300

    improvement on publications URL

--- api/views.py
+++ api/views.py
@@ -14,51 +14,39 @@ from django.utils.dateparse import parse_date
 
 
 class CompanyView(generics.ListCreateAPIView):
-	"""This class defines the create behavior of our rest api."""
 	queryset = Company.objects.all()
 	serializer_class = CompanySerializer
 
 	def perform_create(self, serializer):
-	    """Save the post data when creating a new company."""
 	    serializer.save()
 
 
 class DetailsView(generics.RetrieveUpdateDestroyAPIView):
-	"""This class handles the http GET, PUT and DELETE requests."""
-
 	queryset = Company.objects.all()
 	serializer_class = CompanySerializer
 
 
 class VersionView(generics.ListCreateAPIView):
-	"""This class defines the create behavior of our rest api."""
 	queryset = Version.objects.all()
 	serializer_class = VersionSerializer
 
 	def perform_create(self, serializer):
-	    """Save the post data when creating a new company."""
 	    serializer.save()	
 
 
 class VersionDetailsView(generics.RetrieveUpdateDestroyAPIView):
-	"""This class handles the http GET, PUT and DELETE requests."""
-
 	queryset = Version.objects.all()
 	serializer_class = VersionSerializer
 
 
 class PublicationView(generics.ListCreateAPIView):
-	"""This class defines the create behavior of our rest api."""
 	queryset = Publication.objects.all()
 	serializer_class = PublicationSerializer
 
 	def perform_create(self, serializer):
-	    """Save the post data when creating a new publication."""
 	    serializer.save()
 
-class PublicationDetailsView(generics.RetrieveUpdateDestroyAPIView):
-	"""This class handles the http GET, PUT and DELETE requests."""
-
+class PublicationDetails(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Publication.objects.all()
 	serializer_class = PublicationSerializer
 
